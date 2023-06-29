12e0d8e04f62f292336b1f9e9d93f5e3fe33ea63
anastat1on <anastation.igush@gmail.com>
2023-06-29 15:34:48 -0700

    fix

--- index.js
+++ index.js
@@ -26,31 +26,31 @@ function playSound(soundName){
 function makeSound(letterPressed){
     switch(letterPressed){
         case 'w':
-            playSound('/sounds/crash.mp3')
+            playSound('sounds/crash.mp3')
         break
 
         case 'a':
-            playSound('/sounds/tom-1.mp3')
+            playSound('sounds/tom-1.mp3')
         break
 
         case 's':
-            playSound('/sounds/tom-2.mp3')
+            playSound('sounds/tom-2.mp3')
         break
 
         case 'd':
-            playSound('/sounds/kick-bass.mp3')
+            playSound('sounds/kick-bass.mp3')
         break
 
         case 'j':
-            playSound('/sounds/tom-3.mp3')
+            playSound('sounds/tom-3.mp3')
         break
 
         case 'k':
-            playSound('/sounds/snare.mp3')
+            playSound('sounds/snare.mp3')
         break
 
         case 'l':
-            playSound('/sounds/tom-4.mp3')
+            playSound('sounds/tom-4.mp3')
         break
 
     default: console.log(letterPressed + " letter does not have any sound")
